20dacb4b613d0a5726af8f6d6484f8f759477096
José Márcio Barthem <josemarciobarthem@MacBook-Pro-de-Jose.local>
2023-11-15 14:12:59 -0300

    Feat: php unit

---
+++ tests/Unit/MeuTest.php
@@ -0,0 +1,12 @@
+<?php
+//tests/Unit/MeuTest.php
+use PHPUnit\Framework\TestCase;
+
+class MeuTest extends TestCase {
+    public function testSoma() {
+        $this->assertEquals(4, 2 + 2);
+    }
+
+}
+
+?>
